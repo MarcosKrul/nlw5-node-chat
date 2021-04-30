07bf3cd6bad2874876697a62dd612092746ee473
Marcos Renan Krul <marcoskrul2k@gmail.com>
2021-04-30 15:27:00 -0300

    moduralizacao das configs dos protocolos http e ws

---
+++ src/http.ts
@@ -0,0 +1,32 @@
+import { join } from "path";
+import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
+import "./database";
+import routes from "./routes/index.routes"
+
+const app = express();
+
+app.use(express.static(join(__dirname, "..", "public")));
+app.set("views", join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (req, res) => {
+    return res.render("html/client.html");
+});
+
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+    console.log("Conectado no websocket", socket.id);
+});
+
+app.use(express.json())
+
+app.use(routes);
+
+
+export { http, io };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
-import express from "express";
-import "./database";
-import routes from "./routes/index.routes"
+import { http } from "./http";
+import "./websocket/client";
 
-const app = express();
-
-app.use(express.json())
-
-app.use(routes);
-
-app.listen(process.env.PORT || 3333, () => 
-    console.log(`server ir running at port ${process.env.PORT || 3333}`)
+http.listen(process.env.PORT || 3333, () => 
+    console.log(`server is running at port ${process.env.PORT || 3333}`)
 );
